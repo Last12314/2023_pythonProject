2c04d7e88412ed3165296625ec6a31c21984cf7e
Last12314 <kimdoyun1027@gmail.com>
2023-07-29 10:33:40 +0900

    pythongame

---
+++ pythonBasic/main.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+from pygame.locals import QUIT
+
+pygame.init()
+Surface = pygame.display.set_mode((400,800))
+FPSCLOCK = pygame.time.Clock()
+pygame.display.set_caption("ziboong Window")
+
+def main():
+    while True:
+        Surface.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+if __name__ == '__main__':
+    main()
+5
