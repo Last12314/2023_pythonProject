73390c37be47d71dad0b0c4adf2b27ca72c689a5
Last12314 <kimdoyun1027@gmail.com>
2023-07-29 11:11:10 +0900

    기본 사칙연산 학습

---
+++ pythonBasic/operator.py
@@ -0,0 +1,20 @@
+plus = 5+4
+minus = 10-7
+multiply = 20*3
+division = 9/3
+
+print(plus)
+print(minus)
+
+plus= 10
+print(plus)
+print(multiply)
+print(division)
+
+#에러 케이스
+#divisionZero = 9/0
+
+namuzi = 13%4
+b = 13//4
+print(namuzi)
+print(b)
