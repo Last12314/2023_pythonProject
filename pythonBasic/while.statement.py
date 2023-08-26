88369c7570ac4317f5a0ca38756c24ba0b0a06e6
Last12314 <kimdoyun1027@gmail.com>
2023-08-26 11:38:56 +0900

    random 모듈에서 참조하는 operator 파일과의 충돌 해결

--- pythonBasic/while.statement.py
+++ pythonBasic/while.statement.py
@@ -1,14 +1,14 @@
-num = 0
-
-
-while num < 10:
-    print(num)
-    num += 1
-
-print("====메뉴====")
-print("1. 시작하기")
-print("2. 종료하기")
-print("==========")
+# num = 0
+#
+#
+# while num < 10:
+#     print(num)
+#     num += 1
+#
+# print("====메뉴====")
+# print("1. 시작하기")
+# print("2. 종료하기")
+# print("==========")
 
 # user_input = -1
 #
@@ -26,14 +26,14 @@ print("==========")
 #         break
 
 import random
-answer = random.randrange(0,10)
+answer = random.randrange(0, 10)
 user_input = -1
 
 # 사용자가 answer 맞출때까지 반복
 
 while True:
     user_input = int(input("값을 입력하세요 >>"))
-
     if user_input == answer:
-        print("정답입니다")
-        break
+    print("정답입니다")
+    break
+
